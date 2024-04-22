10b58849483612ecc02a9be7612165893a1afb09
Yassir El yaqine <yel-yaqi@e3r8p5.1337.ma>
2024-04-22 17:17:48 +0100

    list.c additions

--- src/list.c
+++ src/list.c
@@ -6,18 +6,20 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:54:39 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/04/22 17:07:24 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/04/22 17:15:11 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "../include/minishell.h"
 
-void	append(t_token *node, t_token *list)
+void	append(t_token *node, t_token **list)
 {
 	t_token	*prev;
 
-	prev = list->last;
+	if (!list)
+		*list = node;
+	prev = (*list)->last;
 	if (prev)
 		prev->next = node;
 	node->prev = prev;
