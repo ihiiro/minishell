c197e75d1d376fc15158f0b5c2b117ad1bde5e79
Yassir El yaqine <yel-yaqi@e2r9p5.1337.ma>
2024-06-06 16:45:47 +0100

    added ast validity test that checks if tree is structurally correct

--- checks/tests.c
+++ checks/tests.c
@@ -52,7 +52,7 @@ int	main(void)
 	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == OPERATOR);
 	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
 
-	printf("	GOOD\n\n");
+	printf("\033[0;32m	GOOD\033[0m\n\n");
 
 	printf("\n#1 TOKENIZER-TOKEN-NAMES:\n");
 	word_list = ft_split("&& || | > >> << <", " ");
@@ -67,7 +67,7 @@ int	main(void)
 	assert(tokens->next->next->next->next->next->name == HERE_DOC);
 	assert(tokens->next->next->next->next->next->next->name == REDIR_IN);
 
-	printf("	GOOD\n\n");
+	printf("\033[0;32m	GOOD\033[0m\n\n");
 
 	printf("\n#2 PARSER-BUILD-PIPELINES:\n");
 	// simplify redirections before building pipelines (todo)
@@ -78,14 +78,54 @@ int	main(void)
 	assert(build_pipelines(tokens->last) == 13);
 	connect_pipelines(tokens);
 
-	printf("	GOOD\n\n");
+	printf("\033[0;32m	GOOD\033[0m\n\n");
 
 	printf("\n#3 PARSER-FETCH-AST:\n");
 	assert(fetch_ast(tokens)->token->name == AND);
-	printf("	GOOD\n\n");
+	printf("\033[0;32m	GOOD\033[0m\n\n");
 
+	printf("\n#4 AST-VALIDITY ( build_pipelines() + connect_pipelines() + fetch_ast() ):\n\n");
+	word_list = ft_split("cat file0 | grep matchme && ls -la || cd ../..", " ");
+	tokens = NULL;
+	tokenize(word_list, &tokens);
+	name_operators(tokens);
+	build_pipelines(tokens->last);
+	connect_pipelines(tokens);
+	t_ast	*ast = fetch_ast(tokens);
+	printf("ROOT->TOKEN=||");
+	assert(ast->token->name == OR);
+	printf("\n");
+	printf("\033[0;32m	GOOD\033[0m\n");
 
+	printf("ROOT->LEFT->TOKEN=&&");
+	assert(ast->left->token->name == AND);
+	printf("\n");
+	printf("\033[0;32m	GOOD\033[0m\n");
 
+	printf("ROOT->LEFT->RIGHT->TOKEN=ls");
+	assert(strequal(ast->left->right->token->word, "ls"));
+	printf("\n");
+	printf("\033[0;32m	GOOD\033[0m\n");
+
+	printf("ROOT->RIGHT->TOKEN=cd");
+	assert(strequal(ast->right->token->word, "cd"));
+	printf("\n");
+	printf("\033[0;32m	GOOD\033[0m\n");
+
+	printf("ROOT->LEFT->LEFT->TOKEN=|");
+	assert(ast->left->left->token->name == PIPE);
+	printf("\n");
+	printf("\033[0;32m	GOOD\033[0m\n");
+
+	printf("ROOT->LEFT->LEFT->LEFT->TOKEN=cat");
+	assert(strequal(ast->left->left->left->token->word, "cat"));
+	printf("\n");
+	printf("\033[0;32m	GOOD\033[0m\n");
+
+	printf("ROOT->LEFT->LEFT->RIGHT->TOKEN=grep");
+	assert(strequal(ast->left->left->right->token->word, "grep"));
+	printf("\n");
+	printf("\033[0;32m	GOOD\033[0m\n\n");
 
 	printf("PROMPT LOOP FOR DYNAMIC TESTING:\n\n");
 	while (1)
@@ -96,7 +136,7 @@ int	main(void)
 		name_operators(tokens);
 		build_pipelines(tokens->last);
 		connect_pipelines(tokens);
-		// in_order(fetch_ast(tokens));
+		in_order(fetch_ast(tokens));
 		// for (t_token *ptr = tokens; ptr; ptr = ptr->next)
 		// 	printf("%s::%d, ", ptr->word, ptr->type);
 		// printf("\n");
