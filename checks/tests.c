6cf2f20d99c8d27975494aa7bb99e65275fc1e1e
Yassir El yaqine <yel-yaqi@e3r1p15.1337.ma>
2024-06-24 13:40:26 +0100

    modified split.c to not split quoted sequences

--- checks/tests.c
+++ checks/tests.c
@@ -27,164 +27,171 @@ void	in_order(t_ast* root)
 
 int	main(void)
 {
-	printf("\n#0 TOKENIZER-TYPES:\n");
-	char	**word_list = ft_split(" a -l -a&&( b||c -ba | ( f -l	-d -x && g ) )  | d > e	", "	 ");
-	t_token	*tokens = NULL;
-	tokenize(word_list, &tokens);
-	assert(tokens->type == COMMAND);
-	assert(tokens->next->type == ARGUMENT);
-	assert(tokens->next->next->type == ARGUMENT);
-	assert(tokens->next->next->next->type == OPERATOR);
-	assert(tokens->next->next->next->next->type == PARA_OPEN);
-	assert(tokens->next->next->next->next->next->type == COMMAND);
-	assert(tokens->next->next->next->next->next->next->type == OPERATOR);
-	assert(tokens->next->next->next->next->next->next->next->type == COMMAND);
-	assert(tokens->next->next->next->next->next->next->next->next->type == ARGUMENT);
-	assert(tokens->next->next->next->next->next->next->next->next->next->type == OPERATOR);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->type == PARA_OPEN);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->type == ARGUMENT);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->type == ARGUMENT);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == ARGUMENT);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == OPERATOR);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == PARA_CLOSE);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == PARA_CLOSE);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == OPERATOR);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == OPERATOR);
-	assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
 
-	printf("\033[0;32m	GOOD\033[0m\n\n");
+	// printf("\n#0 TOKENIZER-TYPES:\n");
+	// char	**word_list = ft_split(" a -l -a&&( b||c -ba | ( f -l	-d -x && g ) )  | d > e	", "	 ");
+	// t_token	*tokens = NULL;
+	// tokenize(word_list, &tokens);
+	// assert(tokens->type == COMMAND);
+	// assert(tokens->next->type == ARGUMENT);
+	// assert(tokens->next->next->type == ARGUMENT);
+	// assert(tokens->next->next->next->type == OPERATOR);
+	// assert(tokens->next->next->next->next->type == PARA_OPEN);
+	// assert(tokens->next->next->next->next->next->type == COMMAND);
+	// assert(tokens->next->next->next->next->next->next->type == OPERATOR);
+	// assert(tokens->next->next->next->next->next->next->next->type == COMMAND);
+	// assert(tokens->next->next->next->next->next->next->next->next->type == ARGUMENT);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->type == OPERATOR);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->type == PARA_OPEN);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->type == ARGUMENT);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->type == ARGUMENT);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == ARGUMENT);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == OPERATOR);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == PARA_CLOSE);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == PARA_CLOSE);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == OPERATOR);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == OPERATOR);
+	// assert(tokens->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type == COMMAND);
 
-	printf("\n#1 TOKENIZER-TOKEN-NAMES:\n");
-	word_list = ft_split("&& || | > >> << <", " ");
-	tokens = NULL;
-	tokenize(word_list, &tokens);
-	name_no_redir(tokens);
-	name_redirections(tokens);
-	assert(tokens->name == AND);
-	assert(tokens->next->name == OR);
-	assert(tokens->next->next->name == PIPE);
-	assert(tokens->next->next->next->name == REDIR_OUT);
-	assert(tokens->next->next->next->next->name == REDIR_APP);
-	assert(tokens->next->next->next->next->next->name == HERE_DOC);
-	assert(tokens->next->next->next->next->next->next->name == REDIR_IN);
+	// printf("\033[0;32m	GOOD\033[0m\n\n");
 
-	printf("\033[0;32m	GOOD\033[0m\n\n");
+	// printf("\n#1 TOKENIZER-TOKEN-NAMES:\n");
+	// word_list = ft_split("&& || | > >> << <", " ");
+	// tokens = NULL;
+	// tokenize(word_list, &tokens);
+	// name_no_redir(tokens);
+	// name_redirections(tokens);
+	// assert(tokens->name == AND);
+	// assert(tokens->next->name == OR);
+	// assert(tokens->next->next->name == PIPE);
+	// assert(tokens->next->next->next->name == REDIR_OUT);
+	// assert(tokens->next->next->next->next->name == REDIR_APP);
+	// assert(tokens->next->next->next->next->next->name == HERE_DOC);
+	// assert(tokens->next->next->next->next->next->next->name == REDIR_IN);
 
-	printf("\n#2 PARSER-BUILD-PIPELINES:\n");
-	// simplify redirections before building pipelines (todo)
-	word_list = ft_split("cat arg arg arg | ls arg | grep | wc && awk || cd | echo && less | tr", " ");
-	tokens = NULL;
-	tokenize(word_list, &tokens);
-	name_no_redir(tokens);
-	assert(build_pipelines(tokens->last) == 13);
-	connect_pipelines(tokens);
+	// printf("\033[0;32m	GOOD\033[0m\n\n");
 
-	printf("\033[0;32m	GOOD\033[0m\n\n");
+	// printf("\n#2 PARSER-BUILD-PIPELINES:\n");
+	// // simplify redirections before building pipelines (todo)
+	// word_list = ft_split("cat arg arg arg | ls arg | grep | wc && awk || cd | echo && less | tr", " ");
+	// tokens = NULL;
+	// tokenize(word_list, &tokens);
+	// name_no_redir(tokens);
+	// assert(build_pipelines(tokens->last) == 13);
+	// connect_pipelines(tokens);
 
-	printf("\n#3 PARSER-FETCH-AST:\n");
-	assert(fetch_ast(tokens)->token->name == AND);
-	printf("\033[0;32m	GOOD\033[0m\n\n");
+	// printf("\033[0;32m	GOOD\033[0m\n\n");
 
-	printf("\n#4 PARSER-AST-VALIDITY ( build_pipelines() + connect_pipelines() + fetch_ast() ):\n\n");
-	word_list = ft_split("cat file0 | grep matchme && ls -la || cd ../..", " ");
-	tokens = NULL;
-	tokenize(word_list, &tokens);
-	name_no_redir(tokens);
-	build_pipelines(tokens->last);
-	connect_pipelines(tokens);
-	t_ast	*ast = fetch_ast(tokens);
-	printf("ROOT->TOKEN=||");
-	assert(ast->token->name == OR);
-	printf("\n");
-	printf("\033[0;32m	GOOD\033[0m\n");
+	// printf("\n#3 PARSER-FETCH-AST:\n");
+	// assert(fetch_ast(tokens)->token->name == AND);
+	// printf("\033[0;32m	GOOD\033[0m\n\n");
 
-	printf("ROOT->LEFT->TOKEN=&&");
-	assert(ast->left->token->name == AND);
-	printf("\n");
-	printf("\033[0;32m	GOOD\033[0m\n");
+	// printf("\n#4 PARSER-AST-VALIDITY ( build_pipelines() + connect_pipelines() + fetch_ast() ):\n\n");
+	// word_list = ft_split("cat file0 | grep matchme && ls -la || cd ../..", " ");
+	// tokens = NULL;
+	// tokenize(word_list, &tokens);
+	// name_no_redir(tokens);
+	// build_pipelines(tokens->last);
+	// connect_pipelines(tokens);
+	// t_ast	*ast = fetch_ast(tokens);
+	// printf("ROOT->TOKEN=||");
+	// assert(ast->token->name == OR);
+	// printf("\n");
+	// printf("\033[0;32m	GOOD\033[0m\n");
 
-	printf("ROOT->LEFT->RIGHT->TOKEN=ls");
-	assert(strequal(ast->left->right->token->word, "ls"));
-	printf("\n");
-	printf("\033[0;32m	GOOD\033[0m\n");
+	// printf("ROOT->LEFT->TOKEN=&&");
+	// assert(ast->left->token->name == AND);
+	// printf("\n");
+	// printf("\033[0;32m	GOOD\033[0m\n");
 
-	printf("ROOT->RIGHT->TOKEN=cd");
-	assert(strequal(ast->right->token->word, "cd"));
-	printf("\n");
-	printf("\033[0;32m	GOOD\033[0m\n");
+	// printf("ROOT->LEFT->RIGHT->TOKEN=ls");
+	// assert(strequal(ast->left->right->token->word, "ls"));
+	// printf("\n");
+	// printf("\033[0;32m	GOOD\033[0m\n");
 
-	printf("ROOT->LEFT->LEFT->TOKEN=|");
-	assert(ast->left->left->token->name == PIPE);
-	printf("\n");
-	printf("\033[0;32m	GOOD\033[0m\n");
+	// printf("ROOT->RIGHT->TOKEN=cd");
+	// assert(strequal(ast->right->token->word, "cd"));
+	// printf("\n");
+	// printf("\033[0;32m	GOOD\033[0m\n");
 
-	printf("ROOT->LEFT->LEFT->LEFT->TOKEN=cat");
-	assert(strequal(ast->left->left->left->token->word, "cat"));
-	printf("\n");
-	printf("\033[0;32m	GOOD\033[0m\n");
+	// printf("ROOT->LEFT->LEFT->TOKEN=|");
+	// assert(ast->left->left->token->name == PIPE);
+	// printf("\n");
+	// printf("\033[0;32m	GOOD\033[0m\n");
 
-	printf("ROOT->LEFT->LEFT->RIGHT->TOKEN=grep");
-	assert(strequal(ast->left->left->right->token->word, "grep"));
-	printf("\n");
-	printf("\033[0;32m	GOOD\033[0m\n\n");
+	// printf("ROOT->LEFT->LEFT->LEFT->TOKEN=cat");
+	// assert(strequal(ast->left->left->left->token->word, "cat"));
+	// printf("\n");
+	// printf("\033[0;32m	GOOD\033[0m\n");
 
-	printf("#5 PARSER-BUILD-REDIRECTIONS ( build_pipelines() ):\n");
-	tokens = NULL;
-	word_list = ft_split("a > b < c", " ");
-	tokenize(word_list, &tokens);
-	name_no_redir(tokens);
-	build_pipelines(tokens->last);
-	ast = fetch_ast(tokens);
-	name_redirections(tokens);
-	assert(ast->token->name == REDIR_OUT);
-	assert(strequal(ast->left->token->word, "a"));
-	assert(ast->right->token->name == REDIR_IN);
-	assert(strequal(ast->right->right->token->word, "c"));
-	assert(strequal(ast->right->left->token->word, "b"));
-	printf("\033[0;32m	GOOD\033[0m\n\n");
+	// printf("ROOT->LEFT->LEFT->RIGHT->TOKEN=grep");
+	// assert(strequal(ast->left->left->right->token->word, "grep"));
+	// printf("\n");
+	// printf("\033[0;32m	GOOD\033[0m\n\n");
 
-	printf("#6 PARSER-SIMPLIFY-PARA:\n");
-	tokens = NULL;
-	word_list = ft_split("a && (c || d)", " ");
-	tokenize(word_list, &tokens);
-	name_no_redir(tokens);
-	simplify_para(tokens);
-	assert(tokens->type == COMMAND);
-	assert(tokens->next->type == OPERATOR);
-	assert(tokens->next->next->type == PARA);
-	assert(tokens->next->next->subtree->token->name == OR);
-	assert(tokens->next->next->subtree->left->token->type == COMMAND);
-	assert(tokens->next->next->subtree->right->token->type == COMMAND);
-	assert(tokens->next->next->next == NULL);
-	build_pipelines(tokens);
-	connect_pipelines(tokens);
-	connect_para(tokens);
-	printf("\033[0;32m	GOOD\033[0m\n\n");
+	// printf("#5 PARSER-BUILD-REDIRECTIONS ( build_pipelines() ):\n");
+	// tokens = NULL;
+	// word_list = ft_split("a > b < c", " ");
+	// tokenize(word_list, &tokens);
+	// name_no_redir(tokens);
+	// build_pipelines(tokens->last);
+	// ast = fetch_ast(tokens);
+	// name_redirections(tokens);
+	// assert(ast->token->name == REDIR_OUT);
+	// assert(strequal(ast->left->token->word, "a"));
+	// assert(ast->right->token->name == REDIR_IN);
+	// assert(strequal(ast->right->right->token->word, "c"));
+	// assert(strequal(ast->right->left->token->word, "b"));
+	// printf("\033[0;32m	GOOD\033[0m\n\n");
 
-	printf("PROMPT LOOP FOR DYNAMIC TESTING:\n\n");
+	// printf("#6 PARSER-SIMPLIFY-PARA:\n");
+	// tokens = NULL;
+	// word_list = ft_split("a && (c || d)", " ");
+	// tokenize(word_list, &tokens);
+	// name_no_redir(tokens);
+	// simplify_para(tokens);
+	// assert(tokens->type == COMMAND);
+	// assert(tokens->next->type == OPERATOR);
+	// assert(tokens->next->next->type == PARA);
+	// assert(tokens->next->next->subtree->token->name == OR);
+	// assert(tokens->next->next->subtree->left->token->type == COMMAND);
+	// assert(tokens->next->next->subtree->right->token->type == COMMAND);
+	// assert(tokens->next->next->next == NULL);
+	// build_pipelines(tokens);
+	// connect_pipelines(tokens);
+	// connect_para(tokens);
+	// printf("\033[0;32m	GOOD\033[0m\n\n");
 
+	// printf("PROMPT LOOP FOR DYNAMIC TESTING:\n\n");
+
+	// while (1)
+	// {
+	// 	tokens = NULL;
+	// 	word_list = ft_split(readline("\033[1;34mtest> \033[0m"), " ");
+	// 	tokenize(word_list, &tokens);
+	// 	name_no_redir(tokens);
+	// 	tokens = simplify_para(tokens);
+	// 	build_list(tokens);
+	// 	connect_para(tokens);
+	// 	printf("\033[0;32mTree:\033[0m\n");
+	// 	ast = fetch_ast(tokens);
+	// 	visualize_binary_tree(ast);
+	// 	printf("\n");
+	// 	printf("\033[0;32mflattened list:\033[0m\n");
+	// 	for (t_token *ptr = tokens; ptr; ptr = ptr->next)
+	// 		printf("%s ", ptr->word);
+	// 	printf("\n\n");
+	// 	printf("\n");
+	// 	printf("\033[0;32mbottom-top: right-first\033[0m\n");
+	// 	in_order(ast);
+	// }
 	while (1)
 	{
-		tokens = NULL;
-		word_list = ft_split(readline("\033[1;34mtest> \033[0m"), " ");
-		tokenize(word_list, &tokens);
-		name_no_redir(tokens);
-		tokens = simplify_para(tokens);
-		build_list(tokens);
-		connect_para(tokens);
-		printf("\033[0;32mTree:\033[0m\n");
-		ast = fetch_ast(tokens);
-		visualize_binary_tree(ast);
-		printf("\n");
-		printf("\033[0;32mflattened list:\033[0m\n");
-		for (t_token *ptr = tokens; ptr; ptr = ptr->next)
-			printf("%s ", ptr->word);
-		printf("\n\n");
-		printf("\n");
-		printf("\033[0;32mbottom-top: right-first\033[0m\n");
-		in_order(ast);
+		char	**word_list = ft_split(readline("test> "), " 	");
+		for (; *word_list; word_list++)
+			printf("%s\n", *word_list);
 	}
 }
--- src/split/split.c
+++ src/split/split.c
@@ -6,13 +6,13 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 19:45:54 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/04/24 11:23:01 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/06/24 13:35:09 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static int	in_charset(char c, char *charset)
+int	in_charset(char c, char *charset)
 {
 	int	i;
 
@@ -23,6 +23,37 @@ static int	in_charset(char c, char *charset)
 			return (1);
 	}
 	return (0);
+}
+
+#include <stdio.h>
+
+void	skip_quoted(char *str, int *i)
+{
+	if (str[*i] && str[*i] == '"')
+	{
+		*i += 1;
+		while (str[*i] && str[*i] != '"')
+			*i += 1;
+	}
+	else if (str[*i] && str[*i] == '\'')
+	{
+		*i += 1;
+		while (str[*i] && str[*i] != '\'')
+			*i += 1;
+	}
+}
+
+void	count_quoted_len(char *str, int *len, char *charset)
+{
+	char	quote;
+
+	quote = *str;
+	*len += 1;
+	while (str[*len] && str[*len] != quote)
+		*len += 1;
+	*len += 1;
+	while (str[*len] && !in_charset(str[*len], charset))
+		*len += 1;
 }
 
 static int	count_substrs(char *str, char *charset)
@@ -43,7 +74,9 @@ static int	count_substrs(char *str, char *charset)
 		}
 		else if (in_charset(str[i], charset) && !new_string)
 			new_string = 1;
+		skip_quoted(str, &i);
 	}
+	printf("count=%d\n", count);
 	return (count);
 }
 
@@ -57,8 +90,13 @@ static char	*split_strdup(char *str, char *charset)
 	len = 0;
 	i = 0;
 	ptr = str;
-	while (str[len] && !in_charset(str[len], charset))
-		len++;
+	if (*str == '"' || *str == '\'')
+		count_quoted_len(str, &len, charset);
+	else
+	{
+		while (str[len] && !in_charset(str[len], charset))
+			len++;
+	}
 	cpy = (char *)malloc(len + 1);
 	while (i < len)
 	{
@@ -75,22 +113,24 @@ char	**ft_split(char *str, char *charset)
 	char	**substrs;
 	int		index;
 	int		new_string;
+	int		i;
 
 	new_string = 1;
 	index = 0;
+	i = -1;
 	substrs = (char **)malloc(sizeof(char *)
 			* count_substrs(str, charset) + sizeof(char *));
-	while (*str)
+	while (str[++i])
 	{
-		if (!in_charset(*str, charset) && new_string)
+		if (!in_charset(str[i], charset) && new_string)
 		{
-			substrs[index] = split_strdup(str, charset);
+			substrs[index] = split_strdup(&str[i], charset);
 			index++;
 			new_string = 0;
 		}
-		else if (in_charset(*str, charset) && !new_string)
+		else if (in_charset(str[i], charset) && !new_string)
 			new_string = 1;
-		str++;
+		skip_quoted(str, &i);
 	}
 	substrs[index] = 0;
 	return (substrs);
