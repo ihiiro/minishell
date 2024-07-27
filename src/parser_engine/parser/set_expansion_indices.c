a5aa0ef6a2c728aa24832fe3f1498898410e4cea
Yassir El yaqine <yel-yaqi@e3r2p4.1337.ma>
2024-07-27 15:43:00 +0100

    == set_tokens_expansion_indices ==
    simplify two conditions into 1

--- src/parser_engine/parser/set_expansion_indices.c
+++ src/parser_engine/parser/set_expansion_indices.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 12:49:06 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/07/27 15:38:12 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/07/27 15:41:59 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,13 +56,11 @@ void	set_tokens_expansion_indices(t_token *tokens)
 {
 	while (tokens)
 	{
-		if (tokens->type == LIMITER)
+		if (tokens->type == LIMITER && (ft_strchr(tokens->word, '\'')
+			|| ft_strchr(tokens->word, '"')))
 		{
-			if (ft_strchr(tokens->word, '\'') || ft_strchr(tokens->word, '"'))
-			{
-				tokens->expansion_indices = gc_malloc(sizeof(int), COLLECT);
-				tokens->expansion_indices[0] = 1;
-			}
+			tokens->expansion_indices = gc_malloc(sizeof(int), COLLECT);
+			tokens->expansion_indices[0] = 1;
 		}
 		else
 			set_expansion_indices(tokens);
